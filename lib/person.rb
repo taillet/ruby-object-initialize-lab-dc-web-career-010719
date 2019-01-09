1fe85e9342563605a6ba513eb3608d0b0a199f50
Heloise Taillet <ht2394@columbia.edu>
2019-01-09 06:07:59 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,5 @@
 class Person
-  def initialize(name)
-    @name = name
-  end
- 
-  def name=(name)
-    @name = name
-  end
- 
-  def name
-    @name
-  end
+def initialize(name)
+  @name = name
+end
 end
